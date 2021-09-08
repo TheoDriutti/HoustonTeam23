9a01e00284ce1d5988e5ddb0dee2869e5e44711d
Firtnn <HectorBrun@outlook.fr>
2021-09-08 17:58:30 +0200

    InterfaceGameOverAndHealthBar

--- HoustonTeam23/Assets/Scripts/Hector/ShipHealth.cs
+++ HoustonTeam23/Assets/Scripts/Hector/ShipHealth.cs
@@ -8,23 +8,26 @@ public class ShipHealth : MonoBehaviour
     public int currentHealth;
 
     public HealthBar healthBar;
+
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.H))
         {
-            TakeDamage(20);
+            TakeDamage(1);
         }
+        
     }
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
