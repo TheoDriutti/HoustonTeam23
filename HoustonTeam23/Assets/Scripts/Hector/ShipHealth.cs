224face497ec6b64073ddd537bd1cf8e9e91c901
Firtnn <HectorBrun@outlook.fr>
2021-09-08 16:58:10 +0200

    HealthBarV1

---
+++ HoustonTeam23/Assets/Scripts/Hector/ShipHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+}
