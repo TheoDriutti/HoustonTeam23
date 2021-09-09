48c85baf7475efdec9aab9093f3e7f5946de86e6
Firtnn <HectorBrun@outlook.fr>
2021-09-09 16:00:36 +0200

    VfxUpdates

--- HoustonTeam23/Assets/Scripts/Hector/AsteroideDamage.cs
+++ HoustonTeam23/Assets/Scripts/Hector/AsteroideDamage.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class AsteroideDamage : MonoBehaviour
 {
-    public float duration;
-    public float magnitude;
+    public GameObject explosion;
+    public Transform explosionPos;
+
+
+
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            ShipHealth.instance.TakeDamage(1);
-            StartCoroutine(CameraShake.instance.Shake(.10f, .1f));
-            //Desactivé le script qui les font avancé + les remettre dans le pull et au bon transform
+            //ShipHealth.instance.TakeDamage(1);
+            //StartCoroutine(CameraShake.instance.Shake(.10f, .1f));
+            Transform Pos = explosionPos.GetChild(Random.Range(0, explosionPos.childCount));
+            explosion.transform.position = Pos.transform.position;
+            explosion.SetActive(true);
         }
+    }
+
+    public IEnumerator Exploooosion()
+    {
+
+        yield return new WaitForSeconds(1f);
+        explosion.SetActive(false);
     }
 }
